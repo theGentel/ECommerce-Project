eca6b73e1ba0761d1ada397c92ba2536e2fbdf53
Sami Ullah <samiullah@raideit.com>
2022-10-27 10:56:21 -0700

    Changes by sami

--- E_Commerce/Controllers/AboutController.cs
+++ E_Commerce/Controllers/AboutController.cs
@@ -13,5 +13,9 @@ namespace E_Commerce.Controllers
         {
             return View();
         }
+        //public ActionResult AboutAdmin()
+        //{
+        //    return View();
+        //}
     }
 }
